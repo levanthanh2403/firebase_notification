f859fe873d9c2d985a529f10e1e837c47359eb98
Lê Văn Thanh <levanthanh12321@gmail.com>
2025-11-09 15:29:12 +0700

    .

---
+++ FE/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FE/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FE
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
